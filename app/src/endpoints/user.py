2f98fe662436e3ced58cdc39b6ddb7976f630533
freboy1 <dautovalisher33@gmail.com>
2024-02-01 20:27:07 +0600

    Better version

--- app/src/endpoints/user.py
+++ app/src/endpoints/user.py
@@ -1,14 +1,11 @@
 from fastapi import APIRouter
-from app.src.utils.db import wrong_correct, data_return, find_id_user
+from app.src.utils.db import db_get_data, find_id_user
 
 
 router = APIRouter()
-@router.get("/all_data/{user}/{password}/", tags=["User"])
-def all_data(user: str, password: str):
-    if wrong_correct(user, password):
-        return {"message\n": data_return()}
-    else:
-        return {"message": "Wrong password or user name"}
+@router.get("/all_data", tags=["User"])
+def all_data():
+    return {"message\n": db_get_data()}
 
 @router.get("/user/{user_id)", tags=["User"])
 def get_user(id: int):
--- app/src/utils/db.py
+++ app/src/utils/db.py
@@ -1,12 +1,6 @@
 import pymysql
 from app.src.utils.config import host, user, password, db_name
-
-def wrong_correct(query, password2):
-    if query == user and password == password2:
-        return True
-    return False
-
-def data_return():
+def db_get_data():
     connection = pymysql.connect(
         host=host,
         port=3306,
@@ -19,12 +13,8 @@ def data_return():
         with connection.cursor() as cursor:
             show_table = "SELECT * FROM actor"
             cursor.execute(show_table)
-            l = list()
             rows = cursor.fetchall()
-            for row in rows:
-                l.append(row)
-
-            return l
+            return rows
     finally:
         connection.close()
 
@@ -41,11 +31,7 @@ def find_id_user(id):
         with connection.cursor() as cursor:
             show_table = f"SELECT * FROM actor where {id} = actor_id"
             cursor.execute(show_table)
-            l = list()
             rows = cursor.fetchall()
-            for row in rows:
-                l.append(row)
-
-            return l
+            return rows
     finally:
         connection.close()
